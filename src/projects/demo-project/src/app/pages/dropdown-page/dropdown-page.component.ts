619b14e34ee8d0f704fab0db63f24192cc01c283
@Flaeng <flaeng@users.noreply.github.com>
2022-10-09 21:53:11 +0200

    Housekeeping

--- src/projects/demo-project/src/app/pages/dropdown-page/dropdown-page.component.ts
+++ src/projects/demo-project/src/app/pages/dropdown-page/dropdown-page.component.ts
@@ -6,9 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./dropdown-page.component.scss'],
 })
 export class DropdownPageComponent {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   dropdown1: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   dropdown2: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   dropdown3: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   dropdown4: any;
   alert(message: string): void {
     window.alert(message);
