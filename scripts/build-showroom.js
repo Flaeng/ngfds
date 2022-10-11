f78bbf8880611bfd3eed8596c712118da4ad8644
@Flaeng <flaeng@users.noreply.github.com>
2022-10-11 12:31:08 +0200

    Update build-showroom.js

--- scripts/build-showroom.js
+++ scripts/build-showroom.js
@@ -1,7 +1,7 @@
 const { executeAsync } = require("./helpers.js");
 
 (async function () {
-  await executeAsync(`node scripts/icon-packer.js`, './');
-  await executeAsync(`node scripts/generate-docs.js`, './');
+  await executeAsync(`node ../scripts/icon-packer.js`, './src');
+  await executeAsync(`node ../scripts/generate-docs.js`, './src');
   await executeAsync(`ng build demo-project --configuration=production --output-path=../showroom`, 'src');
 })();
