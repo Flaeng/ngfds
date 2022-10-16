b08a08dab2448131399304c3e0ad61392eec5e1d
@Flaeng <flaeng@users.noreply.github.com>
2022-10-16 22:44:02 +0200

    Update public-api.ts

--- src/projects/ngfds/src/lib/modal/public-api.ts
+++ src/projects/ngfds/src/lib/modal/public-api.ts
@@ -2,5 +2,9 @@
  * Public API Surface of ngfds
  */
 
+export * from './modal-content/modal-content.component';
+export * from './modal-footer/modal-footer.component';
+export * from './modal-header/modal-header.component';
+export * from './modal.component';
 export * from './modal.service';
 export * from './modal.module';
