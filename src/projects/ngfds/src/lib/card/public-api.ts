17ed60d4db11ac13e5c4d61d440e3ef84a6acc24
@Flaeng <flaeng@users.noreply.github.com>
2022-10-24 20:19:05 +0200

    Housekeeping

--- src/projects/ngfds/src/lib/card/public-api.ts
+++ src/projects/ngfds/src/lib/card/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ngfds
  */
 
+export * from './card-header/card-header.component';
 export * from './card-content/card-content.component';
 export * from './card.component';
 export * from './card.module';
