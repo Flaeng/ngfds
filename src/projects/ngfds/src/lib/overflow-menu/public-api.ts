87736e40f2d9cafad946d2398cc73075e6fd2da5
@Flaeng <flaeng@users.noreply.github.com>
2022-10-24 20:14:56 +0200

    Housekeeping

--- src/projects/ngfds/src/lib/overflow-menu/public-api.ts
+++ src/projects/ngfds/src/lib/overflow-menu/public-api.ts
@@ -2,5 +2,11 @@
  * Public API Surface of ngfds
  */
 
+export * from './overflow-button/overflow-button.component';
+export * from './overflow-dialog/overflow-dialog.component';
+export * from './overflow-list/overflow-list.component';
+export * from './overflow-list-item/overflow-list-item.component';
+export * from './overflow-sort/overflow-sort.component';
+export * from './overflow-wrapper/overflow-wrapper.component';
 export * from './overflow-menu.component';
 export * from './overflow-menu.module';
