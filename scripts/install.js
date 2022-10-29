083c863353f065c91e8de82ff71edce2d1969404
@Flaeng <flaeng@users.noreply.github.com>
2022-10-29 18:02:33 +0200

    Update install.js

--- scripts/install.js
+++ scripts/install.js
@@ -1,8 +1,8 @@
 const helpers = require("./helpers");
 
 const commandList = [
-  { cmd: "npm ci", path: "./" },
-  { cmd: "npm ci", path: "./src" },
+  { cmd: "npm install", path: "./" },
+  { cmd: "npm install", path: "./src" },
   { cmd: "node ../scripts/custom-types.js", path: "./src" },
 ];
 
