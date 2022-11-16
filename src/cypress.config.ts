5cf3ea97fb5aae22309ed169f75c3709112a10fd
@Flaeng <flaeng@users.noreply.github.com>
2022-11-16 19:03:19 +0100

    Update cypress.config.ts

--- src/cypress.config.ts
+++ src/cypress.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: "angular",
-      bundler: "webpack",
+      framework: 'angular',
+      bundler: 'webpack',
     },
-    specPattern: "**/*.cy.ts",
+    specPattern: '**/*.cy.ts',
   },
 
   component: {
     devServer: {
-      framework: "angular",
-      bundler: "webpack",
+      framework: 'angular',
+      bundler: 'webpack',
     },
-    specPattern: "**/*.cy.ts",
+    specPattern: '**/*.cy.ts',
   },
 
   e2e: {
