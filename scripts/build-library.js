03a0dff76763dc667354b1dd5ae37a495e580338
@Flaeng <flaeng@users.noreply.github.com>
2023-04-17 19:57:35 +0200

    Updated build script

--- scripts/build-library.js
+++ scripts/build-library.js
@@ -38,7 +38,8 @@ const versionNumber = Number.isNaN(versionNo) ? 99 : versionNo;
   );
   const solutionFolder = `${rootFolder}/src`;
   if (versionNo <= 12) {
-    await executeAsync(`npm install jasmine-core@3.8.0`, solutionFolder);
+    // await executeAsync(`npm install jasmine-core@3.8.0`, solutionFolder);
+    await executeAsync(`npm install jasmine-core@4`, solutionFolder);
   }
   await executeAsync(`npm install`, solutionFolder);
 
